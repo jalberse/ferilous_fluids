03e0c9da202ca062302a7e49eddcfd8407e5b5bf
John Alberse <alberse.john@gmail.com>
2022-04-23 12:45:49 -0500

    Set up basic game loop
    Sets up a basic game loop on which to build.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use ggez::*;
+
+struct State {
+    dt: std::time::Duration,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let state = State {
+        dt: std::time::Duration::new(0, 0),
+    };
+
+    let c = conf::Conf::new();
+    let (ctx, event_loop) = ContextBuilder::new("ferilous_fluids", "John Alberse")
+        .default_conf(c)
+        .build()
+        .unwrap();
+
+    event::run(ctx, event_loop, state);
+}
+
+impl ggez::event::EventHandler<GameError> for State {
+    fn update(&mut self, ctx: &mut Context ) -> GameResult {
+        self.dt = timer::delta(ctx);
+        Ok(())
+    }
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        println!("Hello ggez! dt = {}ns", self.dt.as_nanos());
+        Ok(())
+    }
 }
